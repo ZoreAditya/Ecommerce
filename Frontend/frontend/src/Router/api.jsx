4c62d6c024fde5758249b094d376aa2f56432e17
ZoreAditya <113139336+ZoreAditya@users.noreply.github.com>
2025-11-02 17:04:36 +0530

    Update api.jsx

--- Frontend/frontend/src/Router/api.jsx
+++ Frontend/frontend/src/Router/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:9090',
+  baseURL: 'https://ecommerce-3-kky3.onrender.com',
 });
 
 api.interceptors.request.use(
